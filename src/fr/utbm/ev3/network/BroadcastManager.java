eb9d5311c4c639b89cfac81c4db876ef1b96b3cf
Alexandre Lombard <alexandre.lombard@utbm.fr>
2018-12-18 12:41:04 +0100

    Working progress

--- src/fr/utbm/ev3/network/BroadcastManager.java
+++ src/fr/utbm/ev3/network/BroadcastManager.java
@@ -1,10 +1,11 @@
 package fr.utbm.ev3.network;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Broadcast manager ensuring UDP based communication between Lejos EV3 robots and other systems.
@@ -14,6 +15,9 @@ public final class BroadcastManager {
 
     /** The address used for broadcast */
     private static final InetAddress BROADCAST_ADDRESS;
+
+    /** The executor service for the listening task */
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newSingleThreadExecutor();
 
     static {
         try {
@@ -26,15 +30,51 @@ public final class BroadcastManager {
 
     private static BroadcastManager instance;
 
-    private final Map<String, List<MessageEventListener>> listeners = new HashMap<>();
+    private final Map<String, List<MessageEventListener>> listeners = new ConcurrentHashMap<>();
 
     // region Configuration
     private int port = 4242;
     // endregion
 
+    private Runnable listeningTask;
+
     /** Private constructor (singleton pattern) */
     private BroadcastManager() {
-        //
+        this.listeningTask = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    final DatagramSocket socket = new DatagramSocket(5001, InetAddress.getByName("0.0.0.0"));
+                    final byte[] buffer = new byte[10 * 1024];
+
+                    while(true) {
+                        final DatagramPacket packet = new DatagramPacket(buffer,0, buffer.length);
+                        try {
+                            socket.receive(packet);
+
+                            final ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(buffer));
+                            final Object object = ois.readObject();
+
+                            if(object instanceof Message) {
+                                fireMessageReceived((Message) object);
+                            }
+                            // Note: Ignoring unsupported messages
+                        } catch (IOException e) {
+                            // Just in case, the exception will be ignored but logged
+                            System.err.println("Severe (listening): " + e.getMessage());
+                        } catch (ClassNotFoundException e) {
+                            // Just in case, the exception will be ignored but logged
+                            System.err.println("Severe (wrong message): " + e.getMessage());
+                        }
+                    }
+                } catch (SocketException | UnknownHostException e) {
+                    // Note: fatal error
+                    System.err.println("Fatal error (listening): " + e.getMessage());
+                }
+            }
+        };
+
+        EXECUTOR_SERVICE.submit(this.listeningTask);
     }
 
     /**
@@ -112,6 +152,20 @@ public final class BroadcastManager {
         }
 
         return false;
+    }
+
+    /**
+     * Emits the reception of a message
+     * @param message the received message
+     */
+    protected void fireMessageReceived(Message message) {
+        final List<MessageEventListener> topicListeners = listeners.get(message.getTopic());
+
+        if(topicListeners != null) {
+            for(MessageEventListener l : topicListeners) {
+                l.onMessageReceived(message);
+            }
+        }
     }
 
     /**
