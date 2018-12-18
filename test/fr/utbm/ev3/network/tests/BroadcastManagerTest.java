2d2d3457577667ff90717f8505bc1bfa9cd36824
Alexandre Lombard <alexandre.lombard@utbm.fr>
2018-12-18 12:50:14 +0100

    Adding a test class

---
+++ test/fr/utbm/ev3/network/tests/BroadcastManagerTest.java
@@ -0,0 +1,33 @@
+package fr.utbm.ev3.network.tests;
+
+import fr.utbm.ev3.network.BroadcastManager;
+import fr.utbm.ev3.network.Message;
+import fr.utbm.ev3.network.MessageEventListener;
+
+import java.io.IOException;
+
+/**
+ * Test class for the broadcast manager. Note: not a unit test (it's based on the availability of a network interface
+ * supporting broadcast).
+ * @author Alexandre Lombard
+ */
+public class BroadcastManagerTest {
+
+    /**
+     * Main function
+     * @param args command line arguments (ignored)
+     */
+    public static void main(String[] args) throws IOException {
+        final BroadcastManager broadcastManager = BroadcastManager.getInstance();
+
+        broadcastManager.subscribe("hello", new MessageEventListener() {
+            @Override
+            public void onMessageReceived(Message message) {
+                System.out.println(new String(message.getContent()));
+            }
+        });
+
+        broadcastManager.publish(new Message("hello", "world".getBytes()));
+    }
+
+}
